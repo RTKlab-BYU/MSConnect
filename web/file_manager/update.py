6d1144e12c707029d9da1bc4cc4b095ecb28a053
Xiaofeng Xie <xxf-best@hotmail.com>
2023-07-03 16:57:40 -0600

    Fix version number etc for in-app udpate

--- web/file_manager/update.py
+++ web/file_manager/update.py
@@ -1,20 +1,20 @@
 """
 Used to do hot updates of the web server.
 """
-import os
 import subprocess
 import time
 
 
 def update_system():
     subprocess.call(["git", "clone", "https://github.com/RTKlab-BYU/Proteomic-Data-Manager.git", "/home/git_download"])
-    subprocess.call(["git", "-C", "/home/", "rev-parse", "--short", "HEAD", ">>", "/home/web/file_manager/git_version"])
-    subprocess.call(["/venv/bin/python3", "/home/web/manage.py", "collectstatic","--noinput"], cwd="/app/")
+    f = open("/home/git_download/web/file_manager/git_version", "w")
+    subprocess.call(["git", "-C", "/home/git_download", "rev-parse", "--short", "HEAD"], stdout=f)
+    f.close()
+    subprocess.call(["/venv/bin/python3", "/home/git_download/web/manage.py", "collectstatic","--noinput"], cwd="/app/")
     subprocess.call(["rsync", "-a", "-v", "/home/git_download/web/file_manager", "/app/"])
     subprocess.call(["rsync", "-a", "-v", "/home/git_download/web/data_manager", "/app/"])
     subprocess.call(["rsync", "-a", "-v", "/home/git_download/web/schedule_archive", "/app/"])
-    # rm -rf /home/git_download
+    #  rm -rf /home/git_download
     subprocess.call(["rm", "-rf", "/home/git_download"])
-    time.sleep(5)
-
-
+    time.sleep(3)
+    
